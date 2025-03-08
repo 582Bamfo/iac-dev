45762d06a16c7671ee7fc060a22a9b60951f6326
rexfordnana <rexfordnana@gmail.com>
2025-03-08 15:21:20 -0600

    add jenkins-repo

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 resource "aws_iam_user" "devops" {
   name = var.name
-  
+}
+
+resource "aws_ecr_repository" "jenkins-repo" {
+  name                 = "jenkins-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
